546cee595cb5a851762a3db2f0d0d9e50a890317
estrooo <131892691+estrooo@users.noreply.github.com>
2023-09-26 09:42:29 +0300

    Delete practice directory

--- practice/js/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.nav_menu-mobile-button');
-const menu = document.querySelector('.nav_menu-wrapper');
-btn.addEventListener('click', openMenu);
-function openMenu() {
-    menu.classList.toggle('w--open');
-    btn.classList.toggle('menu__btn_active');
-}
